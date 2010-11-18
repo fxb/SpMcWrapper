f12f783cf093ed2661d5d2b9ecbb0d340c00d6d0
Felix Bruns <felixbruns@spotify.com>
2010-11-18 15:31:57 +0100

    Some code to show how to teleport a player.

--- src/de/felixbruns/minecraft/handlers/SpMcChatHandler.java
+++ src/de/felixbruns/minecraft/handlers/SpMcChatHandler.java
@@ -4,11 +4,31 @@ import de.felixbruns.minecraft.SpMcClient;
 import de.felixbruns.minecraft.protocol.ChatColors;
 import de.felixbruns.minecraft.protocol.packets.Packet;
 import de.felixbruns.minecraft.protocol.packets.PacketChatMessage;
+import de.felixbruns.minecraft.protocol.packets.PacketPlayerPosition;
+import de.felixbruns.minecraft.protocol.packets.PacketPlayerPositionAndLook;
 
 public class SpMcChatHandler implements SpMcHandler, ChatColors {
 	public Packet handleClientPacket(SpMcClient client, Packet packet){
 		if(packet instanceof PacketChatMessage){
+			PacketChatMessage chat = (PacketChatMessage)packet;
 			
+			if(chat.message.contains("teleport")){
+				PacketPlayerPositionAndLook pos = new PacketPlayerPositionAndLook();
+				
+				pos.x = 10.0;
+				pos.z = 10.0;
+				pos.yaw = 0.0f;
+				pos.pitch = 0.0f;
+				pos.onGround = false;
+				pos.y = 128.0;
+				pos.stance = 129.62;
+				
+				client.sendToServer(pos);
+				
+				client.sendToClient(pos);
+			}
+			
+			return chat;
 		}
 		
 		return packet;
